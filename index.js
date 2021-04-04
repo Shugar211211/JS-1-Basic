c04c240d594fecd91e15534f69b60dc392a38fab
Shugar211211 <kashey801@gmail.com>
2021-04-04 09:09:04 +0300

    Variables and constants

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 // JavaScript code
 console.log('Hello World !');
+
+// Variables:
+
+// before ES6 - var
+// ES6 - let
+let firstName = 'Mosh';
+let lastName = 'Hamedani';
+console.log(firstName, lastName);
+
+// Cannot be a reserved keyword
+// Should be meaningful
+// Cannot start with a number (1name)
+// Cannot contain a space or hyphen (-)
+// Are case-sensitive
+
+// Constants: const
+
+// If there's no need to reassign, const should be a default choice over let
+const interestRate = 0.3;
+console.log(interestRate);
